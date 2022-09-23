d78205681403455c3138a6a639e858ead1df83ae
Luke Dowling <lukegdowling@gmail.com>
2022-09-23 16:40:44 +1000

    styled create page

--- src/create.js
+++ src/create.js
@@ -6,25 +6,32 @@ import Stack from "@mui/material/Stack";
 function Create() {
   return (
     <div className="Create">
-      <Stack
-        sx={{
-          width: "25ch",
-          paddingBottom: "20px",
-        }}
-        spacing={2}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField label="Event Name" id="event-name" />
-        <TextField label="Host Name" id="host-name" />
-        <TextField label="Start Time" id="start-time" />
-        <TextField label="End Time" id="end-time" />
-        <TextField label="Location" id="location" />
-        <TextField label="Event Photo" id="event-name" />
-      </Stack>
-      <Link to="/event">
-        <button>Next</button>
-      </Link>
+      <div id="create-event-container">
+        <Stack
+          sx={{
+            width: "100%",
+            paddingBottom: "20px",
+          }}
+          spacing={2}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField label="Event Name" id="event-name" />
+          <TextField label="Host Name" id="host-name" />
+          <TextField label="Start Time" id="start-time" />
+          <TextField label="End Time" id="end-time" />
+          <TextField label="Location" id="location" />
+          <div>
+            <label id="photo-label" for="event-photo">
+              Upload Event Photo:
+            </label>
+            <input id="event-photo" type="file" />
+          </div>
+        </Stack>
+        <Link to="/event">
+          <button id="next-button">Next</button>
+        </Link>
+      </div>
     </div>
   );
 }
